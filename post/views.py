54b6dc205be795e893ed1d6939bb4c0e2c9fdbc9
ak4m410x01 <abdallah10kamal@gmail.com>
2023-07-24 02:53:29 +0300

    implement post_list

--- post/views.py
+++ post/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
+from django.http import HttpResponse
+
 
 def post_list(request):
-    pass
+    all_posts = Post.objects.all()
+    return render(request, "post/post_list.html", {"all_posts": all_posts})
+    # return HttpResponse("HEl")
