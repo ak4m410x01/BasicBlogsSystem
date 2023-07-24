55c406b942d6b4363263878df05fb00f055fe1aa
ak4m410x01 <abdallah10kamal@gmail.com>
2023-07-24 23:26:08 +0300

    Implement edit feature

--- post/views.py
+++ post/views.py
@@ -23,3 +23,14 @@ def post_create(request):
     else:
         form = PostForm()
     return render(request, "post/post_create.html", {"form": form})
+
+
+def post_edit(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PostForm(instance=post)
+    return render(request, "post/post_edit.html", {"form": form})
