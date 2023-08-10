258d154edebee0a9522a0811a4a356ec5bec7681
ak4m410x01 <abdallah10kamal@gmail.com>
2023-08-10 04:07:36 +0300

    update some url paths

--- post/urls.py
+++ post/urls.py
@@ -6,14 +6,14 @@ app_name = "blog"
 urlpatterns = [
     # post [list,detail] func
     path("", views.post_list, name="post_list"),
-    path("<int:id>", views.post_detail, name="post_detail"),
+    path("<int:id>/", views.post_detail, name="post_detail"),
     # post [list,detail] func
-    path("create", views.post_create, name="post_create"),
-    path("<int:id>/edit", views.post_edit, name="post_edit"),
+    path("create/", views.post_create, name="post_create"),
+    path("<int:id>/edit/", views.post_edit, name="post_edit"),
     # post [list,detail] cbv
-    path("cbv", views.PostList.as_view(), name="post_list_cbv"),
-    path("cbv/<int:pk>", views.PostDetail.as_view(), name="post_detail_cbv"),
+    path("cbv/", views.PostList.as_view(), name="post_list_cbv"),
+    path("cbv/<int:pk>/", views.PostDetail.as_view(), name="post_detail_cbv"),
     # post [create,update] cbv
-    path("cbv/create", views.PostCreate.as_view(), name="post_create_cbv"),
-    path("cbv/<int:pk>/edit", views.PostUpdate.as_view(), name="post_edit_cbv"),
+    path("cbv/create/", views.PostCreate.as_view(), name="post_create_cbv"),
+    path("cbv/<int:pk>/edit/", views.PostUpdate.as_view(), name="post_edit_cbv"),
 ]
